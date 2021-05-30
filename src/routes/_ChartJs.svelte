ebc18522d17c5e818520da099e8ebbeffa648f1a
Greg Terras <g@terras.com>
2021-05-31 00:29:48 +0200

    add chart.js

---
+++ src/routes/_ChartJs.svelte
@@ -0,0 +1,28 @@
+<script >
+
+	// Chart.js
+	import * as ChartJS from 'chart.js/auto/auto.js'
+	
+	// Chart.js datalabels plugin 
+	import ChartDataLabels from 'chartjs-plugin-datalabels'
+	ChartJS.register(ChartDataLabels)
+	
+	export let options
+
+	const chart = (node, options) => {
+
+		const opts = Object.assign({}, options);
+		const chart = new ChartJS(node, opts)
+
+		return {
+			destroy() {
+				chart.destroy()
+			}
+
+		}
+	}
+
+</script> 
+
+<canvas use:chart={options}></canvas>
+
